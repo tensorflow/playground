2c87f3d0eaba40df4174975ed6e35246493dc25c
Daniel Smilkov <dsmilkov@gmail.com>
2016-11-05 12:24:07 -0400

    moving typescript files to src/ dir

--- src/playground.ts
+++ src/playground.ts
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-/// <reference path="typings/browser.d.ts" />
+/// <reference path="../typings/browser.d.ts" />
 /// <reference path="seedrandom.d.ts" />
 
 import * as nn from "./nn";
