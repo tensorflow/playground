b8fc1d8577b1ef28b2b0eb5607cb346949338da6
Daniel Smilkov <dsmilkov@gmail.com>
2016-11-05 12:31:07 -0400

    extracting analytics to a separate file

---
+++ analytics.js
@@ -0,0 +1,7 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+var ANALYTICS_ID = 'Add your own analytics ID here';
+ga('create', ANALYTICS_ID, 'auto');
